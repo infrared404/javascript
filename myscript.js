2838f471cfa392545bf069682547f521fff5d076
Mas sameera <infrared404@gmail.com>
2020-08-07 23:50:04 +0530

    ajax call and get data

--- myscript.js
+++ myscript.js
@@ -3,3 +3,29 @@ function helloworld(){
 }
 
 setTimeout(helloworld,2000);
+
+$.ajax({
+    url : "http://localhost:3000/EKKO",
+    type: "GET",
+    success : function(result){
+        console.log("Header");
+        console.log(result);
+
+        $.ajax({
+            url : "http://localhost:3000/EKPO?EBELN="+result[0].EBELN,
+            type: "GET",
+            success : function(result){
+                console.log("Header " +result[0].EBELN + " items" );
+                console.log(result);
+            },
+            error : function(error){
+                console.log(error);
+            }
+        })
+
+    },
+    error : function(error){
+        console.log(error);
+    }
+})
+console.log("call done");
